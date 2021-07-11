103457c96bb4d2e667c6f220b13873c0cfd195bb
joinin20190104 <cym2009@naver.com>
2021-07-11 14:21:07 +0900

    third
    resolve the problem that elements' id which is in array history in localStorage and etc..

--- 첫번째 프로젝트/script.js
+++ 첫번째 프로젝트/script.js
@@ -13,41 +13,76 @@ const BALANCE = "balance";
 let history = [];
 let total = 0;
 let incomeTotal = 0,
-  expenseTotal = 0;
+  expenseTotal = 0,
+  liNum = 0;
+let listArr = [];
+
+function saveHistory() {
+  localStorage.setItem(HISTORY, JSON.stringify(history));
+} //계속 숫자가 리셋되면 안됨. 처음에 로드할 때 어떡하냐
 function deleteBtn(event) {
   const btn = event.target;
   const btndiv = btn.parentNode;
   const containerli = btndiv.parentNode;
-  historyLi.removeChild(containerli);
   //그러면 이 태그의 로컬스토리지 내용에서 id를 가져와서
   //div에 저장되어 있는 데이터를 가져옴. ㅓ떻게?
   //이게 핵심이네. 어떻게 로컬 스토리지를 삭제할 것인지.
   let temp = [];
-  const liNum = parseInt(containerli.id);
-  const bringAmount = parseInt(history[liNum].amount);
-
+  liNum = parseInt(containerli.id);
+  historyLi.removeChild(containerli);
+  //console.log(containerli);
+  //console.log("리스트 id가 왜 없는 번호가 나오냐고 " + liNum);
+  const bringAmount = Number(history[liNum].amount);
+  //containerli 리스트를 만들어 리스트아이디도 바꿔줘야되네ㅔ..
+  listArr.splice(liNum, 1);
   if (btndiv.className === "incomeDiv") {
     //income 기록 수정
-    let presentIncome = parseInt(only_income.textContent);
-    presentIncome -= bringAmount;
-    only_income.innerText = `${presentIncome}`;
+    // let presentIncome = parseInt(only_income.textContent);
+    incomeTotal -= bringAmount;
+    only_income.innerText = `${incomeTotal}`;
   } else {
-    let presentExpense = parseInt(only_expense.textContent);
-    presentExpense -= bringAmount;
-    only_expense.innerText = `${presentExpense}`;
+    //let presentExpense = parseInt(only_expense.textContent);
+    expenseTotal -= bringAmount;
+    only_expense.innerText = `${expenseTotal}`;
   }
+  console.log(
+    "지출:",
+    only_expense.textContent,
+    " 수입:",
+    only_income.textContent
+  );
   total -= bringAmount;
   balance.innerText = `${total}₩`;
 
   for (let i = 0; i < history.length; i++) {
     if (parseInt(containerli.id) !== history[i].id) {
       temp.push(history[i]);
-      console.log(containerli.id, history[i].id);
+      //  console.log(containerli.id, history[i].id);
     }
   }
+  //console.log(temp);
 
   //income과 expense 도 바꿔줘야함.
   history = temp;
+
+  //번호 다시 부여하기.
+  saveHistory();
+  resetNum();
+}
+function resetNum() {
+  const loadedHistory = localStorage.getItem(HISTORY);
+  let i = 0;
+  let reset = [];
+  if (loadedHistory !== null) {
+    const parsedHistory = JSON.parse(loadedHistory);
+    parsedHistory.forEach(function (his) {
+      his.id = i;
+      listArr[i].id = i;
+      i++;
+      reset.push(his);
+    });
+  }
+  history = reset;
   saveHistory();
 }
 function paintHistory(historyObj) {
@@ -65,10 +100,11 @@ function paintHistory(historyObj) {
   containerDiv.appendChild(amountSpan);
   li.appendChild(containerDiv);
   li.id = historyObj.id;
+  listArr.push(li);
   historyLi.appendChild(li);
   if (historyObj.amount > 0) {
     incomeTotal += parseInt(historyObj.amount);
-    only_income.innerHTML = incomeTotal;
+    only_income.innerText = incomeTotal;
     containerDiv.className = "incomeDiv";
   } else {
     expenseTotal += parseInt(historyObj.amount);
@@ -79,15 +115,12 @@ function paintHistory(historyObj) {
   //btn가 포함되어 있는 리스트의 id를 구해야 하나..>?
   total += parseInt(historyObj.amount);
 
-  localStorage.setItem(BALANCE, total);
-  balance.innerHTML = total + "₩";
+  balance.innerText = `${total}₩`;
   history.push(historyObj);
+
   saveHistory();
 }
-function saveHistory() {
-  localStorage.setItem(HISTORY, JSON.stringify(history));
-}
-let originNum = 0;
+
 function handleSubmit(event) {
   event.preventDefault();
   const currentText = doText.value;
@@ -95,9 +128,8 @@ function handleSubmit(event) {
   const historyObj = {
     text: currentText,
     amount: currentAmount,
-    id: originNum,
+    id: history.length,
   };
-  originNum++;
   paintHistory(historyObj);
   doText.value = "";
   doAmount.value = "";
@@ -113,7 +145,6 @@ function loadhistory() {
         amount: his.amount,
         id: history.length,
       };
-      console.log(his.text, his.amount, his.id);
       paintHistory(historyObj);
     });
   }
